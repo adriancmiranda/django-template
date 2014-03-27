a058b6c9968f6600a82993b39e1257339a775bc1
Adrian Miranda <adrian.miranda@ppivot.com.br>
2014-03-27 00:55:03 -0300

    Adição de arquivos de configuração

--- application/settings/base.py
+++ application/settings/base.py
@@ -9,3 +9,133 @@ https://docs.djangoproject.com/en/1.6/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
+
+from .utils import rel
+import os, site
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+ROOT_DIR = rel('../..')
+BASE_DIR = rel('..')
+
+# site.addpackage is actually responsible for *.pth file processing
+site.addpackage(os.path.join(ROOT_DIR), 'modules.pth', known_paths=set());
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '2(71@@pf!j$a#u$_y$6vmf-=@z$x2z#q-=(+ono)dt!uu!niol'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = bool(os.environ.get('DEBUG', False))
+TEMPLATE_DEBUG = DEBUG
+ALLOWED_HOSTS = []
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Application definition
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+INSTALLED_APPS = (
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+
+	# Extra
+	'django.contrib.syndication',
+	'django.contrib.flatpages',
+	'django.contrib.comments',
+	'django.contrib.humanize',
+	'django.contrib.sitemaps',
+	'django.contrib.sites',
+
+	# This is South, intelligent schema and data
+	# migrations for ​Django projects.
+	'south',
+
+	# This is {{ project_name }} root module, place where
+	# I set my applications.
+	'application',
+
+	# This is {{ project_name }} news module, place where
+	# I will post my news.
+	'news',
+)
+MIDDLEWARE_CLASSES = (
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+	# Cookie-based, for anonymous users
+	'django.middleware.locale.LocaleMiddleware',
+)
+ROOT_URLCONF = 'application.urls'
+WSGI_APPLICATION = 'application.wsgi.application'
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(ROOT_DIR, 'db.sqlite3'),
+	}
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Templates
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+TEMPLATE_CONTEXT_PROCESSORS = [
+	'django.contrib.auth.context_processors.auth',
+	'django.core.context_processors.debug',
+	'django.core.context_processors.i18n',
+	'django.core.context_processors.media',
+	'django.core.context_processors.static',
+	'django.core.context_processors.tz',
+	'django.core.context_processors.request',
+	'django.contrib.messages.context_processors.messages',
+]
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = [
+	'django.template.loaders.filesystem.Loader',
+	'django.template.loaders.app_directories.Loader',
+	'django.template.loaders.eggs.Loader',
+]
+
+# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+# Always use forward slashes, even on Windows.
+# Don't forget to use absolute paths, not relative paths.
+TEMPLATE_DIRS = [
+	os.path.join(ROOT_DIR, 'templates'),
+]
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+STATICFILES_FINDERS = [
+	'django.contrib.staticfiles.finders.FileSystemFinder',
+	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+GULPFILE = os.path.join(ROOT_DIR, 'static/gulpfile.js')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static_collected')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+	os.path.join(ROOT_DIR, 'static')
+]
--- application/settings/production.py
+++ application/settings/production.py
@@ -1,3 +1,26 @@
 # -*- coding: utf-8 -*-
 
+# Get default settings
 from .base import *
+import dj_database_url
+
+# Feedback
+ADMINS = [
+    ('Me', 'my@mail.com'),
+]
+
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']

247133c7d4f8873b2e73bd5b393c944c72dac628
Adrian Miranda <adriancmiranda@gmail.com>
2014-03-30 22:52:13 -0300

    Update gulpfile.js

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -28,75 +28,184 @@
 	//|
 	//'*/
 	//|**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	//| variables and constants
+	//| variables and constants - (gulpLoadPlugins)
 	//'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	var _ = { app: './app', dist: './dist', tmpl: '../templates' };
 	var $ = gulpLoadPlugins({ pattern: '*', lazy: true });
 
 	//|**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	//| lint
+	//| lint - (plumber, jshint, jscs)
 	//'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	gulp.task('lint', [], function () {
-		// N/A yet.
+	gulp.task('lint', function() {
+		return gulp.src([
+			_.app + '/scripts/**/*.js',
+			'!' + _.app + '/scripts/vendor/**/*.js'
+		])
+		.pipe($.plumber())
+		.pipe($.jshint('.jshintrc'))
+		.pipe($.jshint.reporter('default'))
+		.pipe($.jscs());
 	});
 
 	//|**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	//| scripts
+	//| test - (mocha)
 	//'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	gulp.task('scripts', [], function () {
-		// N/A yet.
+	gulp.task('mocha', function () {
+		gulp.src('test/*.js').pipe($.mocha({ reporter: 'list' }));
 	});
 
 	//|**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	//| images
+	//| scripts - (plumber, size, notify)
 	//'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	gulp.task('images', [], function () {
-		// N/A yet.
+	gulp.task('scripts', ['lint'], function() {
+		return gulp.src([
+			_.app + '/scripts/**/*.js',
+			'!' + _.app + '/scripts/vendor/**/*.js'
+		])
+		.pipe($.plumber())
+		.pipe($.concat('boot.min.js')).pipe($.uglify())
+		.pipe(gulp.dest(_.dist + '/scripts'))
+		.pipe($.size()).pipe($.notify({
+			message: '<%= options.date %> ✓ script: <%= file.relative %>',
+			templateOptions: {
+				date: new Date()
+			}})
+		);
 	});
 
 	//|**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	//| theme
+	//| images - (cache, imagemin, svgmin, size, notify)
 	//'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	gulp.task('theme', [], function () {
-		// N/A yet.
+	gulp.task('images', [], function() {
+		return gulp.src([
+			_.app + '/*.{png,jpg,jpeg,gif,ico}',
+			_.app + '/images/**/*.{png,jpg,jpeg,gif,ico}'
+		]).pipe($.cache($.imagemin({
+			optimizationLevel: 3,
+			progressive: true,
+			interlaced: true
+		}))).pipe(gulp.dest(_.dist)).pipe($.size()).pipe($.notify({
+			message: '<%= options.date %> ✓ image: <%= file.relative %>',
+			templateOptions: {
+				date: new Date()
+			}
+		}));
 	});
 
 	//|**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	//| watch
+	//| theme - (plumber, rubySass, util, autoprefixer, size, notify)
 	//'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	gulp.task('watch', [], function () {
-		// N/A yet.
+	gulp.task('theme', function() {
+		return gulp.src([
+			_.app + '/*.scss',
+			_.app + '/theme/**/*.{sass,scss}'
+		])
+		.pipe($.plumber())
+		.pipe($.rubySass({
+			loadPath: [_.app + '/scripts/vendor'],
+			require: ['sass-css-importer'],
+			style: 'compressed',
+			lineNumbers: true,
+			sourcemap: true,
+			compass: false,
+			trace: true
+		}).on('error', $.util.log))
+		.pipe($.autoprefixer('last 1 version', '> 1%', 'ie 8'))
+		.pipe(gulp.dest(_.dist))
+		.pipe($.size())
+		.pipe($.notify({
+			message: '<%= options.date %> ✓ style: <%= file.relative %>',
+			templateOptions: {
+				date: new Date()
+			}
+		}));
+	});
+
+	//|**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//| Inject bower components - (htmlmin, useref)
+	//'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	gulp.task('bower', [], function () {
+		return gulp.src(_.tmpl + '/*.html')
+		.pipe($.useref())
+		.pipe(gulp.dest(_.dist));
+	});
+
+	//|**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//| watch - (watch)
+	//'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	gulp.task('watch', function() {
+		// Watch for changes in `app` dir
+		gulp.src([
+			_.app + '/*.css',
+			_.app + '/fonts/**/*',
+			_.app + '/scripts/**/*.js',
+			_.app + '/**/*.{png,jpg,jpeg,gif,ico}'
+		], { read: false }).pipe($.watch({}, function(files) {
+			return files;
+		})).pipe($.plumber());
+
+		// Watch .{scss,sass} files
+		$.watch({ glob: [_.app + '/**/*.{scss,sass}'] }, function() {
+			gulp.start('theme');
+		});
+
+		// Watch .js files
+		$.watch({ glob: [_.app + '/scripts/**/*.js'] }, function() {
+			gulp.start('scripts');
+		});
+
+		// Watch image files
+		$.watch({ glob: [_.app + '/**/*.{png,jpg,jpeg,gif,ico}'] }, function() {
+			gulp.start('images');
+		});
+
+		// Watch bower files
+		$.watch({ glob: [_.app + '/scripts/vendor/*'] }, function() {
+			gulp.start('bower');
+		});
+
+		// Launch localhost
+		gulp.start('localhost');
 	});
 
 	//|**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	//| build
 	//'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	gulp.task('build', [], function () {
+	gulp.task('build', ['lint', 'mocha', 'scripts', 'theme', 'images'], function() {
 		// N/A yet.
 	});
 
 	//|**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	//| clean
+	//| clean - (clean)
 	//'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	gulp.task('clean', [], function () {
-		// N/A yet.
+	gulp.task('clean', [], function() {
+		var stream = gulp.src([
+			_.dist + '/scripts',
+			_.dist + '/images',
+			_.dist + '/theme',
+			_.dist + '/fonts'
+		], { read: false });
+		return stream.pipe($.clean());
 	});
 
 	//|**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	//| environ
+	//| environ - (shelljs)
 	//'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	gulp.task('localhost', [], function () {
-		$.shelljs.exec('open https://github.com/adriancmiranda/django-template/');
+	gulp.task('localhost', [], function() {
+		$.shelljs.exec('open http://localhost:8000');
 	});
-	gulp.task('development', function () {
-		$.shelljs.exec('open https://github.com/adriancmiranda/django-template/');
+	gulp.task('development', function() {
+		$.shelljs.exec('open https://github.com/adriancmiranda/django-template/tree/dev');
 	});
-	gulp.task('staging', function () {
+	gulp.task('staging', function() {
 		$.shelljs.exec('open https://github.com/adriancmiranda/django-template/');
 	});
 
 	//|**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	//| alias
 	//'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	gulp.task('default', ['watch']);
+	gulp.task('default', ['clean'], function() {
+		gulp.start('build');
+	});
 
 }(require('gulp'), require('gulp-load-plugins')));
